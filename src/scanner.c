81d77c9b96a1431dea3055d35d914880773959c8
zhamilya <jozefineme@gmail.com>
2018-10-12 17:53:34 +0200

    conditions for symbols and operators

--- src/scanner.c
+++ src/scanner.c
@@ -1,6 +1,58 @@
 #include <stdio.h>
+#include "scanner.h"
 
 int main() {
     printf("asdsss5g!\n");
+
+    /*      here will be cases      */
+
+    //operators and symbols
+    if(c == '+')
+    {
+        state = 20;
+    }
+    if(c == '-')
+    {
+        state = 21;
+    }
+    if(c == '*')
+    {
+        state = 22;
+    }
+    if(c == '%')
+    {
+        state = 23;
+    }
+    if(c == '/')
+    {
+        state = 24;
+    }
+    if(c == '=')
+    {
+        state = 25;
+    }
+    if(c == '{')
+    {
+        state = 26;
+    }
+    if(c == '}')
+    {
+        state = 27;
+    }
+    if(c == ';')
+    {
+        state = 28;
+    }
+    if(c == '(')
+    {
+        state = 29;
+    }
+    if(c == ')')
+    {
+        state = 30;
+    }
+
+
+
     return 0;
 }
