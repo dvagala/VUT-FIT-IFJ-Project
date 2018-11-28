6fc2dd476d39247b7d997614513b7ece315748f5
vinsho <jakub.vins2@gmail.com>
2018-11-28 19:08:52 +0100

    Stack and queue needed for postfix

---
+++ src/postfix_data_managment.h
@@ -0,0 +1,50 @@
+//
+// Created by vinso on 28.11.2018.
+//
+
+#ifndef IFJ_PROJECT_POSTFIX_STACK_H
+#define IFJ_PROJECT_POSTFIX_STACK_H
+
+
+#include "param_list.h"
+#include "scanner.h"
+#include "expressions.h"
+
+typedef struct operator_stack_item{
+    Prec_table_symbols_enum operator;
+    struct operator_stack_item *next;
+}Operator_item;
+
+typedef struct
+{
+    Operator_item *top;
+}Operator_stack;
+
+typedef struct postfix_stack_item{
+    int    *value_int;
+    double *value_double;
+    char  *string;
+    Data_type *type;
+    Prec_table_symbols_enum *operator;
+    bool is_operator;
+    bool is_variable;
+    struct postfix_stack_item *next_item;
+}P_item;
+
+typedef struct {
+    P_item *first;
+    P_item *last;
+}Output_queue;
+
+Operator_stack operator_stack_init();
+bool operator_stack_push(Operator_stack *stack,Prec_table_symbols_enum operator);
+bool operator_pop(Operator_stack *stack);
+bool pop_to_output_queue(Operator_stack *stack, Output_queue *q);
+void operator_stack_free (Operator_stack *stack);
+
+Output_queue queue_inint();
+bool queue_insert(Output_queue *q, bool is_operator, Data_type type, int *int_value, double *float_value, char *string, Prec_table_symbols_enum *operator);
+bool insert_operator(Output_queue *q, Prec_table_symbols_enum operator);
+void queue_dispose(Output_queue *q);
+bool determine_type_and_insert(Output_queue *q, tToken *token);
+#endif //IFJ_PROJECT_POSTFIX_STACK_H
