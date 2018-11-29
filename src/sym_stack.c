a6f4d0dd0d7466d2d0d2294da9e029506ca5ee90
vinsho <jakub.vins2@gmail.com>
2018-11-29 18:55:36 +0100

    fixing stack

--- src/sym_stack.c
+++ src/sym_stack.c
@@ -6,17 +6,15 @@
 #include "stdlib.h"
 
 
-S_stack s_init(){
-    S_stack *stack = malloc(sizeof(S_stack));
+void s_init(S_stack *stack){
+
     stack->top = NULL;
-    return *stack;
 };
 
-bool s_push(S_stack *stack, Prec_table_symbols_enum symbol, Data_type d_type ){
+bool s_push(S_stack *stack, Prec_table_symbols_enum symbol ){
 
     S_item *item = (S_item*)malloc(sizeof(S_item));
     if(!item) return false;
-    item->data_type = d_type;
     item->next_item = stack->top;
     item->symbol = symbol;
     stack->top = item;
@@ -42,21 +40,21 @@ S_item *get_top_terminal(S_stack *stack){
     return NULL;
 }
 
-int get_count_after_stop(S_stack *stack, bool *stop){
+int get_count_after_stop(S_stack stack){
     int count = 0;
-    S_item *item = stack->top;
-    *stop = false;
+    S_item *item = stack.top;
+
     while(item){
         if(item->symbol != P_STOP) {
             count++;
-            item = item->next_item;
         }
         else {
-            *stop = true;
-            break;
+            return count;
         }
+        item = item->next_item;
     }
-    return count;
+
+    return 0;
 }
 void stack_n_pop(S_stack *stack, int n){
     for(int i = 0; i< n; i++ ){
@@ -64,20 +62,21 @@ void stack_n_pop(S_stack *stack, int n){
     }
 }
 
-bool insert_after_top_terminal(S_stack *stack, Prec_table_symbols_enum symbol, Data_type d_type){
+bool insert_after_top_terminal(S_stack *stack, Prec_table_symbols_enum symbol){
     if (stack) {
         S_item *pom = NULL;
         for (S_item *item = stack->top; item != NULL; item = item->next_item) {
             if (item->symbol < P_STOP) {
                 S_item *new = (S_item *) malloc(sizeof(S_item));
+                if(!new)
+                    return false;
                 new->symbol = symbol;
-                new->data_type = d_type;
                 if (pom == NULL) {
+                    new->next_item = stack->top;
                     stack->top = new;
-                    new->next_item = item;
                 } else {
-                    item->next_item = pom->next_item;
-                    pom->next_item = item;
+                    new->next_item = pom->next_item;
+                    pom->next_item = new;
 
                 }
                 return true;
@@ -97,10 +96,16 @@ void s_free (S_stack *stack){
 }
 
 //int main(){
-//    S_stack stack = s_init();
-//    s_push(&stack, P_DOLLAR, type_undefined);
-//    s_push(&stack, P_NON_TERM, type_undefined);
-//    printf("%d", get_top_terminal(&stack)->symbol);
-//    insert_after_top_terminal(&stack, P_ID, type_undefined);
+//    S_stack *stack =  malloc(sizeof(S_stack));
+//    s_init(stack);
+//    s_push(stack, P_DOLLAR);
+//    s_push(stack, P_NON_TERM);
+//    printf("%d",get_top_terminal(stack)->symbol);
+//
+//
+//
+//    printf("%d",get_count_after_stop(*stack));
+////    printf("%d", get_top_terminal(stack)->symbol);
+////    insert_after_top_terminal(stack, P_ID);
 //
 //}
